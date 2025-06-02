a020d04414e4e05154a5c8540259ecc7c37a642b
Nick <nikolay.neykov178@gmail.com>
2025-06-03 00:38:07 +0300

    initial

---
+++ CSV Reader/CSV Reader/cell.hpp
@@ -0,0 +1,17 @@
+#pragma once
+//#include "types.hpp"
+
+class Cell {
+public:
+	//lifecycle
+	Cell();
+	//Cell(const char* value, ColumnType dataType);
+	Cell(const Cell& other);
+	~Cell();
+
+	//getters & setters
+
+private:
+	char* value;
+	//ColumnType dataType;
+};
